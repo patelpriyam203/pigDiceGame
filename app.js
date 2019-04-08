1da55faa89c8d1f7ce894de543d0067941c9664f
PRIYAM PATEL <patelpriyam203@gmail.com>
2019-04-08 09:49:26 -0400

    Added current score to global score class

--- app.js
+++ app.js
@@ -31,18 +31,44 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         // Next Player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        // document.querySelector('.player-0-panel').classList.remove('active');
-        // document.querySelector('.player-1-panel').classList.add('active');
-
-        //Using toggle instead
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
+        nextPlayer();
 
     }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // Add current score to global score
+    score[activePlayer] += roundScore;
+    
+    // update the UI
+    document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];
+
+    // Check if player won the game
+    if (score[activePlayer] >= 100) {
+        document.querySelector("#name-" + activePlayer).textContent = 'Winner';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        // nextPlayer
+        nextPlayer();
+    }
+});
+
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+        
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    //Using toggle instead
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
