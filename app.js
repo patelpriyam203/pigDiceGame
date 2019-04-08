1157c8a7c62fb1bb309e446d59eb03b8ef158d27
PRIYAM PATEL <patelpriyam203@gmail.com>
2019-04-08 06:54:03 -0400

    Finished updating scores and change of active player

--- app.js
+++ app.js
@@ -25,5 +25,24 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDom.src = 'dice-' + dice + '.png';
     
     // Update the round score if the rolled number was not 1
+    if (dice !== 1) {
+        // Add Score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
 
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+        //Using toggle instead
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+    }
 });
